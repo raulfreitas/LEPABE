8f6a95928aeb6e344320e86e3de9a8b7c084531a
raulfreitas <raul.freitas06@gmail.com>
2017-05-22 19:23:50 -0300

    V 6.3

--- Plataforma/assets/js/app.js
+++ Plataforma/assets/js/app.js
@@ -199,43 +199,113 @@ $("#nao-curtiu-perfil").click(function(){
 
 
 
-//chat
-
-$(function(){
-            $(".panel.panel-chat > .panel-heading > .chatMinimize").click(function(){
-                if($(this).parent().parent().hasClass('mini'))
-                {
-                    $(this).parent().parent().removeClass('mini').addClass('normal');
-
-                    $('.panel.panel-chat > .panel-body').animate({height: "250px"}, 500).show();
-
-                    $('.panel.panel-chat > .panel-footer').animate({height: "75px"}, 500).show();
-                }
-                else
-                {
-                    $(this).parent().parent().removeClass('normal').addClass('mini');
-
-                    $('.panel.panel-chat > .panel-body').animate({height: "0"}, 500);
-
-                    $('.panel.panel-chat > .panel-footer').animate({height: "0"}, 500);
-
-                    setTimeout(function() {
-                        $('.panel.panel-chat > .panel-body').hide();
-                        $('.panel.panel-chat > .panel-footer').hide();
-                    }, 500);
 
 
-                }
 
-            });
-            $(".panel.panel-chat > .panel-heading > .chatClose").click(function(){
-                $(this).parent().parent().remove();
-            });
+ 	//this function can remove a array element.
+			Array.remove = function(array, from, to) {
+  				var rest = array.slice((to || from) + 1 || array.length);
+  				array.length = from < 0 ? array.length + from : from;
+  				return array.push.apply(array, rest);
+			};
+
+			//this variable represents the total number of popups can be displayed according to the viewport width
+			var total_popups = 0;
+
+			//arrays of popups ids
+			var popups = [];
+
+			//this is used to close a popup
+			function close_popup(id)
+			{
+				for(var iii = 0; iii < popups.length; iii++)
+				{
+					if(id == popups[iii])
+					{
+						Array.remove(popups, iii);
+						document.getElementById(id).style.display = "none";
+						calculate_popups();
+						return;
+					}
+				}
+			}
+
+			//displays the popups. Displays based on the maximum number of popups that can be displayed on the current viewport width
+			function display_popups()
+			{
+				var right = 220;
+
+				var iii = 0;
+				for(iii; iii < total_popups; iii++)
+				{
+					if(popups[iii] != undefined)
+					{
+						var element = document.getElementById(popups[iii]);
+						element.style.right = right + "px";
+						right = right + 320;
+						element.style.display = "block";
+					}
+				}
+
+				for(var jjj = iii; jjj < popups.length; jjj++)
+				{
+					var element = document.getElementById(popups[jjj]);
+					element.style.display = "none";
+				}
+			}
+
+			//creates markup for a new popup. Adds the id to popups array.
+			function register_popup(id, name)
+			{
+
+				for(var iii = 0; iii < popups.length; iii++)
+				{
+					//already registered. Bring it to front.
+					if(id == popups[iii])
+					{
+						Array.remove(popups, iii);
+
+						popups.unshift(id);
+
+						calculate_popups();
 
 
-			$(".conversar").click(function(){
-				$("#box-chat").show();
-			})
+						return;
+					}
+				}
 
+				var element = '<div class="popup-box" id="'+ id +'">';
+				element = element + '<div class="popup-head">';
+				element = element + '<div class="popup-head-left">'+ name +'</div>';
+				element = element + '<div class="popup-head-right"><a href="javascript:close_popup(\''+ id +'\');">&#10005;</a></div>';
+				element = element + '<div style="clear: both"></div></div><div class="panel-body"><div class="messageMe"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" alt=""/><span>Lorem ipsum dolor sit amet, consectetur adipiscing</span><div class="clearFix"></div></div><div class="messageHer"><img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" alt=""/><span>Lorem ipsum dolor sit amet, consectetur adipiscing</span><div class="clearFix"></div></div><div class="panel-footer"><textarea name="textMessage" cols="0" rows="0"></textarea><button type="button" class="btn btn-envia-box">Enviar</button></div></div></div>';
 
- })
+				$("body").append(element);
+				popups.unshift(id);
+
+				calculate_popups();
+
+			}
+
+			//calculate the total number of popups suitable and then populate the toatal_popups variable.
+			function calculate_popups()
+			{
+				var width = window.innerWidth;
+				if(width < 540)
+				{
+					total_popups = 0;
+				}
+				else
+				{
+					width = width - 200;
+					//320 is width of a single popup box
+					total_popups = parseInt(width/320);
+				}
+
+				display_popups();
+
+			}
+
+			//recalculate when window is loaded and also when window is resized.
+			window.addEventListener("resize", calculate_popups);
+			window.addEventListener("load", calculate_popups);
